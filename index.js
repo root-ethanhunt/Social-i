01551fbadcbbe9d2c4d91bcbc6c7ba7168f62fcf
VIKASH KUMAR <vikash.hunt111@gmail.com>
2021-03-24 04:31:11 +0530

    added template engine

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+// use express router
+app.use('/',require('./routes'))
+
+// set up the view engine
+app.set('view engine','ejs')
+app.set('views','./views')
+
 
 app.listen(port,()=>{
-    console.log(`Server is running on port:${port}`)
-})
+    console.log(`Server is running on port : ${port}`)
+}) 
