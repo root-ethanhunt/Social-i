7446e225ee2f87767fd753da8cac72728ffdc61d
VIKASH KUMAR <vikash.hunt111@gmail.com>
2022-04-23 13:02:37 +0530

    package_Updated_again

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,19 +1,17 @@
-const mongoose = require('mongoose')
-const env = require('./environment') 
+const mongoose = require("mongoose");
+const env = require("./environment");
 
 //const url =`mongodb://127.0.0.1:27017/${env.db}`
-const url = env.db
+const url = env.db;
 
-mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
-
 
 module.exports = db;
