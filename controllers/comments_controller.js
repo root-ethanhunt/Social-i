73601d5edfc5977f3efd433132bbca50e7ef8ed5
VIKASH KUMAR <vikash.hunt111@gmail.com>
2022-04-23 17:23:23 +0530

    error resolver

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,97 +1,85 @@
-const Comment = require('../models/comment')
-const Post = require('../models/post')
-const commentMailer = require('../mailers/comments_mailer')
-const Like = require('../models/like')
-const User = require('../models/user')
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+const commentMailer = require("../mailers/comments_mailer");
+const Like = require("../models/like");
+const User = require("../models/user");
 
 module.exports.create = async function (req, res) {
-
   try {
-    let post = await Post.findById(req.body.post)
+    let post = await Post.findById(req.body.post);
 
     if (post) {
       let comment = await Comment.create({
         content: req.body.content,
         post: req.body.post,
-        user: req.user._id
-      })
-      post.comments.push(comment)
-      post.save()
+        user: req.user._id,
+      });
+      post.comments.push(comment);
+      post.save();
 
-      comment = await comment.populate('user', 'name email').execPopulate()
+      comment = await comment.populate("user", "name email").execPopulate;
       // commentMailer.newComment(comment)
       //console.log(comment.user.name)
 
       if (req.xhr) {
-
         // Similar for comments to fetch the user's id!
         // comment = await comment.populate('user', 'name').execPopulate()
 
         return res.status(200).json({
           data: {
-            comment: comment
+            comment: comment,
           },
-          message: "Post created!"
-        })
+          message: "Post created!",
+        });
       }
 
+      req.flash("success", "Comment created");
 
-      req.flash('success', 'Comment created')
-
-      return res.redirect('/')
-
+      return res.redirect("/");
     }
   } catch (err) {
-    console.log('Error', err)
-    return
+    console.log("Error", err);
+    return;
   }
-
-
-}
+};
 
 module.exports.destroy = async function (req, res) {
   try {
-
-    let comment = await Comment.findById(req.params.id)
+    let comment = await Comment.findById(req.params.id);
     // console.log(req.params.id)
     //console.log(comment.post)
     if (comment.user == req.user.id) {
       // console.log(req.user.id)
-      let postId = comment.post
+      let postId = comment.post;
 
-      comment.remove()
+      comment.remove();
 
-      let post = Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } })
-
+      let post = Post.findByIdAndUpdate(postId, {
+        $pull: { comments: req.params.id },
+      });
 
       // CHANGE :: destroy the associated likes for this comment
-      await Like.deleteMany({ likeable: comment._id, onModel: 'Comment' });
-
-
+      await Like.deleteMany({ likeable: comment._id, onModel: "Comment" });
 
       if (req.xhr) {
         return res.status(200).json({
           data: {
-            comment_id: req.params.id
+            comment_id: req.params.id,
           },
-          message: 'comment deleted'
-        })
+          message: "comment deleted",
+        });
       }
 
-      req.flash('success', 'Comment deleted!')
+      req.flash("success", "Comment deleted!");
 
-      return res.redirect('back')
+      return res.redirect("back");
+    } else {
+      req.flash("error", "Unauthorized");
+      return res.redirect("back");
     }
-    else {
-      req.flash('error', 'Unauthorized')
-      return res.redirect('back')
-    }
+  } catch (err) {
+    console.log("Error", err);
+    req.flash("Error", err);
+    return;
   }
-  catch (err) {
-    console.log('Error', err)
-    req.flash('Error', err)
-    return
-  }
-
-
-}
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,78 +1,67 @@
-const Post = require('../models/post')
-const Comment = require('../models/comment')
-const Like = require('../models/like')
-module.exports.create = async function(req,res){
-    // res.end('<h1>Posts</h1>')
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+const Like = require("../models/like");
+module.exports.create = async function (req, res) {
+  // res.end('<h1>Posts</h1>')
 
-    try{
-      let post = await Post.create({
-            content:req.body.content,
-            user: req.user._id
-        })
+  try {
+    let post = await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
 
-        if(req.xhr){
+    if (req.xhr) {
+      // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+      post = await post.populate("user", "name").execPopulate;
 
-           // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
-           post = await post.populate('user', 'name').execPopulate()
-
-            return res.status(200).json({
-                data:{
-                    post:post
-                },
-                message:"Post created!"
-            })
-        }
-        req.flash('success','Post created')
-         return res.redirect('back')
-
-    }catch(err){
-        res.flash('error',err)
-       // console.log('Error',err)
-        return res.redirect('back')
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "Post created!",
+      });
     }
-  
-    
-}
+    req.flash("success", "Post created");
+    return res.redirect("back");
+  } catch (err) {
+    req.flash("Error", err);
 
+    return res.redirect("back");
+  }
+};
 
-module.exports.destroy = async function(req,res){
-
-try{
-    let post = await Post.findById(req.params.id)
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
 
     // .id means converting the object id into string
-   if(post.user == req.user.id){
+    if (post.user == req.user.id) {
+      // CHANGE :: delete the associated likes for the post and all its comments' likes too
+      await Like.deleteMany({ likeable: post, onModel: "Post" });
+      await Like.deleteMany({ _id: { $in: post.comments } });
 
-     // CHANGE :: delete the associated likes for the post and all its comments' likes too
-     await Like.deleteMany({likeable:post, onModel:'Post'})
-     await Like.deleteMany({_id:{$in: post.comments}})
+      post.remove();
 
-       post.remove()
+      await Comment.deleteMany({ post: req.params.id });
 
-      await Comment.deleteMany({post:req.params.id})
-
-      if(req.xhr){
-          return res.status(200).json({
-              data:{
-                  post_id:req.params.id
-              },
-              message:'post deleted'
-          })
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post_id: req.params.id,
+          },
+          message: "post deleted",
+        });
       }
 
-      req.flash('success','Post and associated comments deleted')
+      req.flash("success", "Post and associated comments deleted");
 
-      return res.redirect('back')
-   }
-   else{
-    req.flash('error','You cannot delete Post')
-       return res.redirect('back')
-   }
-}catch(err){
-
-   req.flash('Error',err)
-    return res.redirect('back')
-}
-
-  
-}
+      return res.redirect("back");
+    } else {
+      req.flash("error", "You cannot delete Post");
+      return res.redirect("back");
+    }
+  } catch (err) {
+    req.flash("Error", err);
+    return res.redirect("back");
+  }
+};
